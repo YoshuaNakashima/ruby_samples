1471917607dbe033d8bf493671252ceb5b2e8cce
yoshua <yoshua.n.j@gmail.com>
2018-11-08 02:24:41 +0900

    pass spec

--- sample001_triangle/lib/triangle.rb
+++ sample001_triangle/lib/triangle.rb
@@ -1,5 +1,42 @@
 class Triangle
-  def self.check()
-    return true
+  def self.check(a, b, c)
+    na = int_or_nil(a)
+    nb = int_or_nil(b)
+    nc = int_or_nil(c)
+
+    if na.nil? || nb.nil? || nc.nil?
+      return "引数は、全て整数のみ入力してください。"
+    end
+
+    # 判定順序に依存しています。
+    if triangle?(na, nb, nc)
+      if equilateral?(na, nb, nc)
+        return "正三角形ですね！"
+      elsif isosceles?(na, nb, nc)
+        return "二辺三角形ですね！"
+      else
+        return "不等辺三角形ですね！"
+      end
+    else
+      return "三角形じゃないです＞＜"
+    end
   end
+
+  def self.int_or_nil(a)
+    /(?<na>\d+)/ =~ a.to_s()
+    return na == nil ? nil : na.to_i()
+  end
+  def self.equilateral?(a, b, c)
+    return a == b && b == c
+  end
+  def self.isosceles?(a, b, c)
+    return (a == b && a != c) || (b == c && b != a) || (c == a && c != b)
+  end
+  def self.triangle?(a, b, c)
+    return a < b + c && b < c + a && c < a + b
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts Triangle.check(ARGV[0], ARGV[1], ARGV[2])
 end
--- sample001_triangle/spec/lib/triangle_spec.rb
+++ sample001_triangle/spec/lib/triangle_spec.rb
@@ -1,5 +1,59 @@
 require File.expand_path('lib/triangle')
 
 describe Triangle do
-  specify { expect(Triangle.check()).to eq true }
+
+  # コマンドライン引数は、すべて文字列だそうです。
+  specify { expect(Triangle.check("a,", "3,", "4")).to eq "引数は、全て整数のみ入力してください。" }
+  specify { expect(Triangle.check("2,", "a,", "4")).to eq "引数は、全て整数のみ入力してください。" }
+  specify { expect(Triangle.check("2,", "3,", "a")).to eq "引数は、全て整数のみ入力してください。" }
+  specify { expect(Triangle.check("2,", "3,", "4")).to eq "不等辺三角形ですね！" }
+  specify { expect(Triangle.check("2,", "2,", "1")).to eq "二辺三角形ですね！" }
+  specify { expect(Triangle.check("1,", "1,", "1")).to eq "正三角形ですね！" }
+  specify { expect(Triangle.check("1,", "2,", "3")).to eq "三角形じゃないです＞＜" }
+
+  specify { expect(Triangle.check(2, 3, 4)).to eq "不等辺三角形ですね！" }
+  specify { expect(Triangle.check(2, 2, 1)).to eq "二辺三角形ですね！" }
+  specify { expect(Triangle.check(1, 1, 1)).to eq "正三角形ですね！" }
+  specify { expect(Triangle.check(1, 2, 3)).to eq "三角形じゃないです＞＜" }
+
+  specify { expect(Triangle.triangle?(1, 1, 1)).to eq true }
+  specify { expect(Triangle.triangle?(2, 3, 4)).to eq true }
+  specify { expect(Triangle.triangle?(1, 2, 3)).to eq false }
+  specify { expect(Triangle.triangle?("2", "3", "4")).to eq false }
+
+  specify { expect(Triangle.isosceles?(2, 2, 1)).to eq true }
+  specify { expect(Triangle.isosceles?(2, 3, 4)).to eq false }
+  specify { expect(Triangle.isosceles?(2, 2, 2)).to eq false }
+  specify { expect(Triangle.isosceles?("2", "3", "4")).to eq false }
+
+  specify { expect(Triangle.equilateral?(2, 2, 2)).to eq true }
+  specify { expect(Triangle.equilateral?(2, 3, 4)).to eq false }
+  specify { expect(Triangle.equilateral?(2, 2, 1)).to eq false }
+  specify { expect(Triangle.equilateral?("2", "3", "4")).to eq false }
+
+  specify { expect(Triangle.triangle?( 3,  4,  5)).to eq true }
+  specify { expect(Triangle.triangle?( 5, 12, 13)).to eq true }
+  specify { expect(Triangle.triangle?( 7, 24, 25)).to eq true }
+  specify { expect(Triangle.triangle?( 9, 40, 41)).to eq true }
+  specify { expect(Triangle.triangle?(11, 60, 61)).to eq true }
+  specify { expect(Triangle.triangle?(13, 84, 85)).to eq true }
+  specify { expect(Triangle.triangle?(15,  8, 17)).to eq true }
+  specify { expect(Triangle.triangle?(15,112,113)).to eq true }
+  specify { expect(Triangle.triangle?(17,144,145)).to eq true }
+  specify { expect(Triangle.triangle?(19,180,181)).to eq true }
+  specify { expect(Triangle.triangle?(21, 20, 29)).to eq true }
+  specify { expect(Triangle.triangle?(27, 36, 45)).to eq true }
+  specify { expect(Triangle.triangle?(33, 56, 65)).to eq true }
+  specify { expect(Triangle.triangle?(35, 12, 37)).to eq true }
+  specify { expect(Triangle.triangle?(39, 80, 89)).to eq true }
+  specify { expect(Triangle.triangle?(45, 28, 53)).to eq true }
+  specify { expect(Triangle.triangle?(45,108,117)).to eq true }
+  specify { expect(Triangle.triangle?(51,140,149)).to eq true }
+  specify { expect(Triangle.triangle?(55, 48, 73)).to eq true }
+  specify { expect(Triangle.triangle?(63, 16, 65)).to eq true }
+  specify { expect(Triangle.triangle?(65, 72, 97)).to eq true }
+  specify { expect(Triangle.triangle?(75,100,125)).to eq true }
+  specify { expect(Triangle.triangle?(77, 36, 85)).to eq true }
+  specify { expect(Triangle.triangle?(91, 60,109)).to eq true }
+  specify { expect(Triangle.triangle?(99, 20,101)).to eq true }  
 end
